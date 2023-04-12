316f35479a4a0d128a5f057fb3b0fa8b49624f20
Domineice <33221103+Domineice@users.noreply.github.com>
2023-04-12 21:30:40 +0700

    back deploy

--- assign1/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// import './assets/main.css'
-import VueCookies from 'vue3-cookies'
-
-createApp(App).use(router).use(VueCookies, {
-    expireTimes: "1d"
-}).mount('#app')
