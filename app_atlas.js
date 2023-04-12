824b2deb22f8b03fbdb1dfa1da78df22edfdc8bf
Domineice <33221103+Domineice@users.noreply.github.com>
2023-04-13 00:27:20 +0700

    edit connect

--- app_atlas.js
+++ app_atlas.js
@@ -25,7 +25,7 @@ let collection, collectionAuth;
 async function dbConnect() {
   console.log("DB connect Started");
   // console.log(url)
-  await client.connect(url,{ useUnifiedTopology: true });
+  await client.connect();
   console.log("DB connect successful");
 
   db = client.db(dbName);
