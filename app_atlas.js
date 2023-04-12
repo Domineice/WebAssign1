0cff5cc2b3ecfa8cec90a96e938791b86eefd01e
Domineice <33221103+Domineice@users.noreply.github.com>
2023-04-13 00:23:28 +0700

    changemongo

--- app_atlas.js
+++ app_atlas.js
@@ -1,29 +1,31 @@
 const express = require("express");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cors());
 
 // Connection URL
 
 const { MongoClient, Collection } = require("mongodb");
-const url = "mongodb+srv://contact:YRIAFRQR1NFIcsHM@contactlist.c2xlua1.mongodb.net/?retryWrites=true&w=majority";
-
-// mongodb+srv://contact:YRIAFRQR1NFIcsHM@contactlist.c2xlua1.mongodb.net/?retryWrites=true&w=majority
+const url =
+  "mongodb+srv://Contact:z0AIncfgH7nKiNQp@contactlist.mlnvgaz.mongodb.net/?retryWrites=true&w=majority";
+  
+  
 const client = new MongoClient(url);
-
-const cors = require('cors')
+const cors = require("cors");
 
 // Database Name
 const dbName = "ContactList";
 let db;
-let collection,collectionAuth;
-
-app.use(cors())
+let collection, collectionAuth;
 
 async function dbConnect() {
-  await client.connect();
+  console.log("DB connect Started");
+  // console.log(url)
+  await client.connect(url,{ useUnifiedTopology: true });
   console.log("DB connect successful");
 
   db = client.db(dbName);
@@ -32,9 +34,9 @@ async function dbConnect() {
   return "done.";
 }
 
-async function getAuth(user,pass) {
+async function getAuth(user, pass) {
   // console.log(user + " " + pass)
-  var query = { Username: user ,Password : pass};
+  var query = { Username: user, Password: pass };
   const findResult = await collectionAuth.count(query);
   return parseInt(findResult);
 }
@@ -52,7 +54,7 @@ async function showById(id) {
 
 async function deleteById(id) {
   var query = { cid: id };
-  const deleteResult = await collection.deleteOne(query)
+  const deleteResult = await collection.deleteOne(query);
   return deleteResult;
 }
 
@@ -66,8 +68,8 @@ async function updateById(id, editContacts) {
       email: editContacts.email,
       mobile: editContacts.mobile,
       facebook: editContacts.facebook,
-      imageUrl: editContacts.imageUrl
-    }
+      imageUrl: editContacts.imageUrl,
+    },
   };
 
   const insertResult = await collection.updateOne(query, newData);
@@ -79,77 +81,145 @@ async function showAll() {
   return allData;
 }
 
-dbConnect().catch(console.error);
+// dbConnect().catch(console.error);
 
-app.post("/logincontact/", function (req, res) {
-  const user = req.body;
-  console.log(user)
-  getAuth(user.Username,user.Password)
-    .then((result) => {
-      if(parseInt(result)!=0){
-        const tolen = jwt.sign({ Username: user.Username }, 'secretkey');
-        console.log(tolen)
-        res.json({ token: tolen });
-      }
-      else{
-        res.status(401).json({ error: 'Invalid username or password' });
-      }
-    })
-    .catch(console.error);
-});
+// app.post("/logincontact/", function (req, res) {
+//   const user = req.body;
+//   console.log(user);
+//   getAuth(user.Username, user.Password)
+//     .then((result) => {
+//       if (parseInt(result) != 0) {
+//         const tolen = jwt.sign({ Username: user.Username }, "secretkey");
+//         console.log(tolen);
+//         res.json({ token: tolen });
+//       } else {
+//         res.status(401).json({ error: "Invalid username or password" });
+//       }
+//     })
+//     .catch(console.error);
+// });
 
-app.post("/contacts/", function (req, res) {
-  addContect(req.body)
-    .then((result) => {
-      console.log("Add success");
-      res.json(result);
-    })
-    .catch(console.error);
-});
+// app.post("/contacts/", function (req, res) {
+//   addContect(req.body)
+//     .then((result) => {
+//       console.log("Add success");
+//       res.json(result);
+//     })
+//     .catch(console.error);
+// });
 
-app.get("/contacts/", function (req, res) {
-  showAll()
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch(console.error);
-});
+// app.get("/contacts/", function (req, res) {
+//   showAll()
+//     .then((result) => {
+//       console.log(result);
+//       res.json(result);
+//     })
+//     .catch(console.error);
+// });
 
-app.get("/contacts/:id", function (req, res) {
-  showById(req.params.id, req.body)
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch(console.error);
-});
+// app.get("/contacts/:id", function (req, res) {
+//   showById(req.params.id, req.body)
+//     .then((result) => {
+//       console.log(result);
+//       res.json(result);
+//     })
+//     .catch(console.error);
+// });
 
-app.post("/contacts/:id", function (req, res) {
-  updateById(req.params.id,req.body)
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch(console.error);
-});
+// app.post("/contacts/:id", function (req, res) {
+//   updateById(req.params.id, req.body)
+//     .then((result) => {
+//       console.log(result);
+//       res.json(result);
+//     })
+//     .catch(console.error);
+// });
 
-app.delete("/contacts/:id", function (req, res) {
-  deleteById(req.params.id)
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch(console.error);
-});
+// app.delete("/contacts/:id", function (req, res) {
+//   deleteById(req.params.id)
+//     .then((result) => {
+//       console.log(result);
+//       res.json(result);
+//     })
+//     .catch(console.error);
+// });
 
-const PORT = process.env.PORT || 5005;
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-    console.log('Deploying...');
-  });
+dbConnect()
+  .then(() => {
+    const PORT = process.env.PORT || 5005;
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-  // deploy
+    app.use(cors());
+
+    app.post("/logincontact/", function (req, res) {
+      const user = req.body;
+      console.log(user);
+      getAuth(user.Username, user.Password)
+        .then((result) => {
+          if (parseInt(result) != 0) {
+            const tolen = jwt.sign({ Username: user.Username }, "secretkey");
+            console.log(tolen);
+            res.json({ token: tolen });
+          } else {
+            res.status(401).json({ error: "Invalid username or password" });
+          }
+        })
+        .catch(console.error);
+    });
+
+    app.post("/contacts/", function (req, res) {
+      addContect(req.body)
+        .then((result) => {
+          console.log("Add success");
+          res.json(result);
+        })
+        .catch(console.error);
+    });
+
+    app.get("/contacts/", function (req, res) {
+      showAll()
+        .then((result) => {
+          console.log(result);
+          res.json(result);
+        })
+        .catch(console.error);
+    });
+
+    app.get("/contacts/:id", function (req, res) {
+      showById(req.params.id, req.body)
+        .then((result) => {
+          console.log(result);
+          res.json(result);
+        })
+        .catch(console.error);
+    });
+
+    app.post("/contacts/:id", function (req, res) {
+      updateById(req.params.id, req.body)
+        .then((result) => {
+          console.log(result);
+          res.json(result);
+        })
+        .catch(console.error);
+    });
+
+    app.delete("/contacts/:id", function (req, res) {
+      deleteById(req.params.id)
+        .then((result) => {
+          console.log(result);
+          res.json(result);
+        })
+        .catch(console.error);
+    });
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+      console.log("Deploying...");
+    });
+  })
+  .catch(console.error);
+
+// deploy
 
 // var server = app.listen(5005, function () {
 //   const host = "127.0.0.1";
